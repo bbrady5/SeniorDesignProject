9266c1b15b1570478ac45473751abea10f077c18
bbrady5 <53125769+bbrady5@users.noreply.github.com>
2019-09-19 18:39:46 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -1,14 +1,78 @@
 #!/usr/bin/env python
 
 import socket
+import os
 
-host='127.0.0.1'
-port=5000
+
+host='10.137.76.191'
+port=8080
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host , port))
+s.connect((host, port))
 
-f = open("../pic_c.png", "rb")
+f = open("penguin1.jpg", "rb")
+
+while True:
+
+    veri = f.read(512)
+    print("herefirst")
+    if not veri:
+        print("errhere1")
+        break
+    s.send(veri)
+    print("512 bytes sent")
+
+f.close()
+
+print("picture sent?")
+
+s.close()
+
+
+
+'''
+# host='127.0.0.1'
+host='10.137.76.191'
+port=8080
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+directory="/Users/BredaMarie/PycharmProjects/Test2_TCP/jpgpics/"
+try:
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg"):
+            print("here1")
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("here1.2")
+            s.connect((host, port))
+            print(filename)
+            filer = "/Users/BredaMarie/PycharmProjects/Test2_TCP/jpgpics/" + filename
+            f = open(filer, "rb")
+            print("here2")
+            while True:
+
+                veri = f.read(5120)
+                if not veri:
+                    break
+                s.send(veri)
+
+            f.close()
+            print("picture sent")
+            print("Success")
+            s.close()
+            print("here3")
+            continue
+        else:
+            print("Error")
+            continue
+except Exception as e:
+    print(e)
+    print("error collecting files")
+
+
+
+
+
 
 while True:
 
@@ -19,6 +83,8 @@ while True:
 
 f.close()
 
-print("picture sent?")
+print("picture sent")
 
-s.close()
+#s.close()
+
+'''
