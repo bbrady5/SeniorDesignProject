c07233e01cbf1b3acf4dcf985ef50c540cf70c05
bbrady5 <53125769+bbrady5@users.noreply.github.com>
2019-09-30 23:50:28 -0400

    Add files via upload

--- cam_script.py
+++ cam_script.py
@@ -1,6 +1,131 @@
 # Cam_script - By: Brady - Sat Sep 7 2019
-import sensor, image, pyb, time, uos
+import sensor, image, time, network, usocket, sys, uos, pyb
 
+
+# AP info
+SSID='VUPlay' # Network SSID
+KEY='vuplay123'  # Network key
+#SSID='Villanova Senior Design - wirele' # Network SSID
+#KEY='merakipassword'  # Network key
+
+HOST=''
+PORT=8005
+
+
+while True:
+    # Init wlan module and connect to network
+    print("Trying to connect... (may take a while)...")
+
+    wlan = network.WINC()
+    wlan.connect(SSID, key=KEY, security=wlan.WPA_PSK)
+
+    # We should have a valid IP now via DHCP
+    configure = wlan.ifconfig()
+    print(configure)
+    #retry if get an invalid IP address
+    if(configure[4] != '0.0.0.0'):
+        break
+
+
+'''
+#removing all images from image to send
+            directory = "c:\\Faces\\Images_to_send"
+            #coming from os.listdir() sorted alphabetically
+            for filename in os.listdir(directory):
+                if filename.endswith(".jpg"):
+                    os.remove(directory + "\\" + filename)
+                    #print("remove SUCCESS")
+                    continue
+                else:
+                    print("ERROR2")
+                    continue
+'''
+
+
+s = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+print("about to bind")
+s.bind((HOST, PORT))
+print("about to listen")
+s.listen(0)
+print("listened")
+# receive text file
+conn, address = s.accept()
+print(conn)
+print(address)
+print("connection")
+filename= "faces_txt.txt"
+f = open(filename, "wb") # renamed file
+print("opened file")
+while True:
+    try:
+        veri = conn.recv(512)
+        print("received face bytes")
+        f.write(veri)
+        print("text written to file")
+    except Exception as e:
+        print(e)
+        print("no more bytes")
+        break
+
+f.close()
+conn.close()
+s.close()
+print("connection/connection closed")
+print("text file closed.")
+#read file and separate into list
+f = open(filename, "r") #rb
+lines = f.read().splitlines()
+n=len(lines)
+print("lines = "+ str(n))
+print(lines)
+
+print("about to declare")
+# receive images
+i=1
+m=0
+uos.chdir("/Rec_faces")
+
+while m<n:
+    print("entered while loop")
+    s = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+    print("about to bind")
+    PORT +=1
+    print(PORT)
+    s.bind((HOST, PORT))
+    print("about to listen")
+    s.listen(0)
+    print("listened")
+
+    conn, address = s.accept()
+    print(conn)
+    print(address)
+    print("connection")
+    filepicname= lines[m] #how does it know its a string?
+    print(filepicname)
+    f = open(filepicname, "wb") # renamed file
+    print("opened file")
+    while True:
+        try:
+            veri = conn.recv(2000)
+            print("received 512 bytes")
+            f.write(veri)
+            print("picture written to file")
+        except Exception as e:
+            print(e)
+            print("no more bytes")
+            break
+
+    f.close()
+    print("picture closed.")
+    i+=1
+    m+=1
+    conn.close()
+    print("connection closed")
+    #conn.close()
+    #print("connection closed")
+    s.close()
+    print("socket closed")
+    time.sleep(100)
 
 
 
@@ -148,4 +273,22 @@ print(min(lbp_unique_list))
 name_ind = lbp_unique_list.index(min(lbp_unique_list))
 print(name_ind)
 print(unique_list[name_ind])
-print("The person you are looking at is: " + unique_list[name_ind])
+id_name = "The person you are looking at is: " + unique_list[name_ind]
+print(id_name)
+
+
+#set up client connection to phone
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#host2
+#port2
+c.connect((host2, port2))
+try:
+    c.send((idname).encode())
+    print("face id text sent")
+except Exception as e:
+    print(e)
+    print("error sending face id string")
+c.close()
+
+
